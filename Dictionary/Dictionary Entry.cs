faee4b0772468e3ee3f8b78a8b13ac0241f7bf87
poddubnyy@ukr.net <poddubnyy@ukr.net>
2017-11-13 18:00:27 +0300

    13.11.17 Homework made.

---
+++ Dictionary/Dictionary Entry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class DictionaryEntryVerbs
+    {
+        private string Form1 { get; }
+        private string Form2 { get; }
+        private string Form3 { get; }
+        private string Translation { get; }
+        public DictionaryEntryVerbs(string _form1, string _form2, string _form3, string _translation)
+        {
+            Form1 = _form1;
+            Form2 = _form2;
+            Form3 = _form3;
+            Translation = _translation;
+        }
+        public override string ToString()
+        {
+            return $"{Form1}\t{Form2}\t{Form3}\t{Translation}";
+        }
+    }
+}
--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -13,6 +13,19 @@ namespace Dictionary
             //Пример использования репозитария
             IrregularVerbsRepository repo = new IrregularVerbsRepository();
             var verbs = repo.GetWords();
+            Dictionary<string, DictionaryEntryVerbs> DictionVerbs =
+              new Dictionary<string, DictionaryEntryVerbs>();
+            for(int i=0; i < verbs.Length-1; i++)
+            DictionVerbs.Add(verbs[i][0], new DictionaryEntryVerbs(verbs[i][0], verbs[i][1], verbs[i][2], verbs[i][3]));
+            do
+            {
+                Console.WriteLine("Enter the verb");
+                bool Found = DictionVerbs.TryGetValue(Console.ReadLine(), out DictionaryEntryVerbs value);
+                if(!Found) Console.WriteLine("Not found verb!");
+                else
+                Console.WriteLine(value.ToString());
+                Console.WriteLine("Set esc for exit");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
         }
     }
 }
